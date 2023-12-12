d972f558d999c526abe93e74fda05ec2bbff613d
RHETT CHASE <rhettjchase@gmail.com>
2023-12-11 18:47:08 -0800

    updates seed books with image URLs

--- models/book.js
+++ models/book.js
@@ -6,6 +6,7 @@ const bookSchema = new Schema({
   title: String,
   description: String,
   status: String,
+  image_url: String,
 });
 
 const Book = mongoose.model('Book', bookSchema);
--- seed.js
+++ seed.js
@@ -11,20 +11,20 @@ async function seed() {
     {
       title: 'The Hunger Games',
       description: 'In the ruins of a place once known as North America lies the nation of Panem, a shining Capitol surrounded by twelve outlying districts. The Capitol is harsh and cruel and keeps the districts in line by forcing them all to send one boy and one girl between the ages of twelve and eighteen to participate in the annual Hunger Games, a fight to the death on live TV.',
-      status: 'Unavailable',
+      status: 'Available',
       image_url: 'https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1447303603l/2767052.jpg',
     },
     {
       title: 'Wheel of Time',
       description: 'The Eye of the World',
       status: 'Available',
-      image_url: 'https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1555447414l/228665._SY475_.jpg',
+      image_url: 'https://m.media-amazon.com/images/M/MV5BZjcyOTk2YmQtMTI3NS00MTM3LWIyZDgtZGJkMDk1YmE2ZDEwXkEyXkFqcGdeQXVyMzQ2MDI5NjU@._V1_.jpg',
     },
     {
       title: 'Harry Potter and the Sorcerers Stone',
       description: 'An orphaned boy enrolls in a school of wizardry, where he learns the truth about himself, his family and the terrible evil that haunts the magical world',
       status: 'Available',
-      image_url: 'https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1474154022l/3._SY475_.jpg',
+      image_url: 'https://m.media-amazon.com/images/W/MEDIAX_792452-T2/images/I/81iqZ2HHD-L._AC_UF1000,1000_QL80_.jpg',
     },
     {
       title: 'Twilight',
@@ -36,13 +36,13 @@ async function seed() {
       title: 'The Return of the King',
       description: 'The Grey Havens',
       status: 'Available',
-      image_url: 'https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1298411339l/34031.jpg',
+      image_url: 'https://m.media-amazon.com/images/I/41KPaTTRxFL.jpg',
     },
     {
       title: 'Harry Potter and the Chamber of Secrets',
       description: 'The Heir of Slytherin',
       status: 'Unavailable',
-      image_url: 'https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1474169725l/15881._SY475_.jpg',
+      image_url: 'https://m.media-amazon.com/images/W/MEDIAX_792452-T2/images/I/510CXXt9CqL.jpg',
     },
     {
       title: 'Lord of the Flies',
